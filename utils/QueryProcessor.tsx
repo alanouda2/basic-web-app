9f501bc5e1ad14387316fb78ecfa0e4bd94b9432
alanouda2 <alanoud2@andrew.cmu.edu>
2023-10-01 12:24:35 +0300

    Tests and New Query

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -5,6 +5,14 @@ export default function QueryProcessor(query: string): string {
       "English poet, playwright, and actor, widely regarded as the greatest " +
       "writer in the English language and the world's pre-eminent dramatist."
     );
+    
+    
+  }
+  if (query.toLowerCase().includes("andrew")) {
+    return ("alanoud2"
+    );
+    
+    
   }
 
   return "";
